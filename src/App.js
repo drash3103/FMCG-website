9a0dc540b8c4316152a7bd9508b745ba12476d55
Het Pastagia <21bt04061@gsfcuniversity.ac.in>
2023-11-14 22:08:57 +0530

    Teesra Commitment

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ const App = () => {
         </div>
       </section>
 
-      <section className="about">
+      {/* <section className="about">
         <h2> About FMCG</h2>
         <p>
           Welcome to FMCG, your trusted source for premium liquid cleaning products in India. We take pride in our commitment to quality and our unique approach to sourcing these products. Unlike conventional brands, we partner with small-scale industries spread across the length and breadth of India, supporting local businesses while delivering superior cleaning solutions to your doorstep. Our extensive range includes specialized liquid detergents for bathrooms, floors, and toilets, ensuring your home or workspace stays sparkling clean. We also offer powerful phenyls and car shampoos for a holistic cleaning experience.
@@ -62,7 +62,29 @@ const App = () => {
 
           Join us on our journey to create a cleaner, greener, and more prosperous India. FMCG - where quality, convenience, and community come together to redefine your cleaning experience.
         </p>
-      </section>
+      </section> */}
+      <section className="about">
+  <div className="about-left">
+  <div className="about-left1">
+    <h2>ABOUT</h2>
+  </div>
+  <div className="about-left2">
+    <h2>US</h2>
+  </div>
+ </div>
+  
+  <div className="about-right">
+    <p>
+     Welcome to FMCG, your trusted source for premium liquid cleaning products in India. We take pride in our commitment to quality and our unique approach to sourcing these products. Unlike conventional brands, we partner with small-scale industries spread across the length and breadth of India, supporting local businesses while delivering superior cleaning solutions to your doorstep. Our extensive range includes specialized liquid detergents for bathrooms, floors, and toilets, ensuring your home or workspace stays sparkling clean. We also offer powerful phenyls and car shampoos for a holistic cleaning experience.
+
+      At FMCG, quality is our top priority. We rigorously test and vet all our products, guaranteeing their effectiveness, safety for your family, and their eco-friendliness. Our e-commerce platform makes shopping easy and convenient, allowing you to explore our products and make your purchases online. By choosing FMCG, you're not only investing in top-notch cleaning solutions but also contributing to the growth of small-scale industries and the well-being of local communities.
+
+      Join us on our journey to create a cleaner, greener, and more prosperous India. FMCG - where quality, convenience, and community come together to redefine your cleaning experience.
+    </p>
+  </div>
+</section>
+
+
       <section className="Product">
       <div class="wrapper">
   <div class="container">
@@ -75,11 +97,7 @@ const App = () => {
         </div>
       </div>
       <div class="right">
-        <button class="btn btn-sm">
-        <h1>
-          BUY
-        </h1>
-        </button>
+      <button class="btn" role="button">BUY</button>
       </div>
     </div>
   </div>
@@ -235,12 +253,35 @@ const App = () => {
 </div>
 
 </section>
- <section className='Service'>
-    <h1> Make a service form</h1>
+<section className="Service">
+      <div className="left-box">
+        <form>
+          <label htmlFor="customerName">Customer Name:</label>
+          <input type="text" id="customerName" name="customerName" />
 
- </section>
+          <label htmlFor="companyName">Company Name:</label>
+          <input type="text" id="companyName" name="companyName" />
+
+          <label htmlFor="productDescription">Brief Description:</label>
+          <textarea id="productDescription" name="productDescription" rows="4"></textarea>
+
+          <label htmlFor="serviceDate">Select Date for Service:</label>
+          <input type="date" id="serviceDate" name="serviceDate" />
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+
+      <div className="right-box">
+        <img src="Service.gif" alt="Service Image" />
+      </div>
+    </section>
  <section className='Footer'>
-    <h1>Add icons for insta , whatsapp and a quer page with nodemailer integrated</h1>
+    <div class="block1"><h1>Taneesk</h1></div>
+    <div class="block2"><h1>Drashti</h1></div>
+    <div class="block3"><h1>Shamik</h1></div>
+    <div class="block4"><h1>Khushi</h1></div>
+    <div class="block5"><h1>Het</h1></div>
  </section>
     </div>
   );
